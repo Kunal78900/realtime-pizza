0794f3ef79aa6648151a8257810c756fcc0434e0
kunal7557 <kunal3558.be20@chitkara.edu.in>
2023-02-07 02:18:33 +0530

    Server created

---
+++ server.js
@@ -0,0 +1,31 @@
+const express =  require('express');
+const app = express();
+const ejs = require('ejs');
+const path = require('path');
+const expressLayout = require('express-ejs-layouts');
+
+
+const PORT = process.env.PORT || 8000;
+
+app.get('/',function(req,res){
+    res.render('home');
+})
+
+
+//setting up template engine
+
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'));
+app.set('view engine','ejs');
+
+
+
+
+
+app.listen(PORT,function(err){
+    if(err){
+     console.log("Falied to connect to server");
+     return;
+    }
+    console.log("Server is running at PORT no.",PORT);
+})
